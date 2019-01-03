dff136888f5f7d93616b5c3f5d33bf693d0c156e
Oluwatosin Folaranmi <foluwatosin@ITILP_L014.simplexsystems.com>
2019-01-03 19:43:28 +0100

    schema update

---
+++ server/models/projects.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose')
+
+const projectSchema = new mongoose.Schema({
+    name: { type: String },
+    description: { type: String },
+    isActive: { type: Boolean, default: true }
+})
---
+++ server/models/teamMember.js
@@ -0,0 +1,5 @@
+const mongoose = require('mongoose')
+
+const teamMemberSchema = new mongoose.Schema({
+    name: {  type: String }
+})
