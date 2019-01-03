fa558cdce7fbc35620b8879d631cb085cafd4b95
Oluwatosin Folaranmi <foluwatosin@ITILP_L014.simplexsystems.com>
2019-01-03 19:17:46 +0100

    First commit

---
+++ server/server/api/routes/projects.js
@@ -0,0 +1,6 @@
+module.exports = function (router) {
+    // GET: List of active projects
+    router.get('/projects', function (req, res) {
+
+    })
+}
---
+++ server/server/api/routes/standup.js
@@ -0,0 +1,6 @@
+module.exports = function (router) {
+    // GET: List of active standup
+    router.get('/standup', function (req, res) {
+
+    })
+}
